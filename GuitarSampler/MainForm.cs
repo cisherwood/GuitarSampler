c0d03fa9f75773fe13d735fc1340e1dc436d07f0
ChrisPCA <cisherwood@p-ca.com>
2019-10-17 15:24:35 -0400

    Add project files.

---
+++ GuitarSampler/MainForm.cs
@@ -0,0 +1,60 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuitarSampler
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            for (int n = -1; n < WaveOut.DeviceCount; n++)
+            {
+                var caps = WaveOut.GetCapabilities(n);
+                Console.WriteLine($"{n}: {caps.ProductName}");
+            }
+
+            for (int n = -1; n < WaveIn.DeviceCount; n++)
+            {
+                var caps = WaveIn.GetCapabilities(n);
+                Console.WriteLine($"{n}: {caps.ProductName}");
+            }
+
+            foreach (var asio in AsioOut.GetDriverNames())
+            {
+                Console.WriteLine(asio);
+            }
+
+
+
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
